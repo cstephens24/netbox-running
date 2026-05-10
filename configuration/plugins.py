ccf7c5efaa254bdd3bee19a668a94ed58e465354
Charles <cstephens24@gmail.com>
2026-05-10 05:54:20 +0000

    now has netbox diode enabled.

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -13,5 +13,13 @@ PLUGINS_CONFIG = {
     'netbox_topology_views': {
         'allow_coordinates_saving': True,
         'always_save_coordinates': True,
-    }
+    },
+    "netbox_diode_plugin": {
+        # Diode gRPC target for communication with Diode server
+        "diode_target_override": "grpc://10.0.1.63:8080/diode",
+        # NetBox username associated with changes applied via plugin
+        "diode_username": "diode",
+        # netbox-to-diode client secret
+        "netbox_to_diode_client_secret": "bSEW2pcisC11IiQpbaI96Qs8CunziLDgCIttD3Zgs=",
+    },
 }
